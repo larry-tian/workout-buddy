0f568e42a4254e6db2c7d73431abea18f632604b
Larry-Tian <ganlin@uw.edu>
2020-08-16 02:11:18 -0700

    Fixed some styling

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -8,20 +8,21 @@ class Plan extends React.Component {
       {title: "Waist Slimming" , type: "Beginner" , duration: "30 min"},
       {title: "Yoga" , type: "Advanced" , duration: "20 min"},
       {title: "Planks", type: "Group workout", duration: "1 min"},
+      {title: "Mountain Climing", type: "Group workout", duration: "2 min"},
     ]
     return (
       <Container fluid>
         <Row className="mt-5 mb-3"><h1>This Week's Plan</h1></Row>
         <Row className="mb-3">
           {days.map(day => (
-            <Col><Button>{day}</Button></Col>
+            <Col><Button className="mb-3">{day}</Button></Col>
           ))}
         </Row>
         <Row className="mh-100">
           {workouts.map(workout => (
             <Col key={workout.title}>
               <Card className="h-px">
-                <CardTitle className="text-left m-3">
+                <CardTitle className="text-left m-3 mb-2 mt-2">
                   {workout.title}
                 </CardTitle>
                 <CardBody>
